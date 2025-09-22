abbbf35e663004c85456753e33154e9c9c8f7857
Kirke Kadarik <kirke.kadarik@tptlive.ee>
2025-09-22 15:09:58 +0300

    easy type ex

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
+let num = 1;
+console.log(num);
+console.log(typeof num);
+num = 1.2;
+console.log(num);
+console.log(typeof num);
+let text = 'hello world';
+console.log(text);
+console.log(typeof text);
+
+ text = 'h'; //char
+console.log(text);
+console.log(typeof text);
+
+let bool = true;
+console.log(bool);
+console.log(typeof bool);
+
+bool = false;
+console.log(bool);
+console.log(typeof bool);
+
+let undef = undefined;
+console.log(undef);
+console.log(typeof undef);
+
+let nil = null;
+console.log(nil);
+console.log(typeof nil);
+
+let array = [1, 2, 'hello', 'world', true, [1, 2, 3]];
+console.log(array);
+console.log(typeof array);
+
+let obj = {
+    name: 'Kaspar',
+    age: '32',
+    hairColor: 'Blonde',
+    isMale: true,
+};
+console.log(obj);
+console.log(typeof obj);
 
 
-console.log(foo)
-console.log(bar)
-
-var foo = 'hello'; // old way dont use! hoisting
-let bar = 'world';
