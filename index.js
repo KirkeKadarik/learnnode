a158c3a08a40d89d234f33d6e39d09775203df8b
Kirke Kadarik <kirke.kadarik@tptlive.ee>
2025-09-25 16:11:17 +0300

    add if and switch ex.

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
-let bool = true;
-console.log(bool);
-bool = false;
-console.log(bool);
-bool = true && false; //AND
-console.log(bool);
-bool = true || false; //OR
-bool = false || true; //OR
-bool = false || false; //OR
-bool = true || true; //OR
+let num = 10;
+if(num > 10) {
+    console.log('bigger than 10');
+} else if (num === 10) {
+    console.log('equal to 10');
+}
+ else {
+    console.log('smaller than 10');
+}
+let day = new Date("2025-09-25").getDay();
+console.log(day);
 
-console.log(bool);
-bool = !true; //NOT
-console.log(bool);
-bool = !false; //NOT
-console.log(bool);
-bool = (true && false || true) && !(false || true && false);
-console.log(bool);
-bool = 10 > 5;
-bool = 10 < 5;
-bool = 10 > 10;
-bool = 10 < 10;
-bool = 10 == 10;
-bool = '10' == 10;
-bool = '10' === 10; //non strict will convert back
-bool = 10 >= 10;
-bool = 10 <= 10;
-bool = 10 != 10;
-bool = '10' != 10;
-bool = '10' !== 10;
-bool = [] == '';
-bool = [1, 2, 3] == 1;
+if (day === 1) {
+    console.log('Esmaspäev');
+} else if (day === 2) {
+    console.log('Teisipäev');
+} else if (day === 3) {
+    console.log('Kolmapäev');
+} else if (day === 4) {
+    console.log('Neiljapäev');
+} else if (day === 5 || day === 6) {
+    console.log('Pidupäev');
+}  else if (day === 0) {
+    console.log('Pühapäev');
+} else {
+    console.log('Imelikpäev');
+}
 
-console.log(bool);
+switch(day) {
+    case 1:
+        comsole.log('esmaspäev');
+        break;
+           case 2:
+        comsole.log('teisipäev');
+        break; 
+          case 3:
+        comsole.log('kolmapäev');
+        break;
+           case 4:
+        comsole.log('neljapäev');
+        break;
+           case 5:
+           case 6:
+        comsole.log('pidupäev');
+        break;
+           case 7:
+        comsole.log('pühaäev');
+        break;
+        default:
+            console.log('imelikpäev');
+}
+ 
