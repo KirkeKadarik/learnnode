79c16aeceb48dc636b33a8658aa43832629eadc0
Kirke Kadarik <kirke.kadarik@tptlive.ee>
2025-09-25 15:32:36 +0300

    add bool ex.

--- index.js
+++ index.js
@@ -1,67 +1,34 @@
-function hello() {
-    console.log('hello world');
+let bool = true;
+console.log(bool);
+bool = false;
+console.log(bool);
+bool = true && false; //AND
+console.log(bool);
+bool = true || false; //OR
+bool = false || true; //OR
+bool = false || false; //OR
+bool = true || true; //OR
 
-}
+console.log(bool);
+bool = !true; //NOT
+console.log(bool);
+bool = !false; //NOT
+console.log(bool);
+bool = (true && false || true) && !(false || true && false);
+console.log(bool);
+bool = 10 > 5;
+bool = 10 < 5;
+bool = 10 > 10;
+bool = 10 < 10;
+bool = 10 == 10;
+bool = '10' == 10;
+bool = '10' === 10; //non strict will convert back
+bool = 10 >= 10;
+bool = 10 <= 10;
+bool = 10 != 10;
+bool = '10' != 10;
+bool = '10' !== 10;
+bool = [] == '';
+bool = [1, 2, 3] == 1;
 
-hello();
-hello();
-hello();
-hello();
-hello();
-
-function helloName(name) {
-    console.log(`hello ${name}`);
-
-}
-
-helloName('Kaspar');
-helloName('Marta');
-
-function hi(name = 'who are you', age = 'what') {
-    console.log(`Hi ${name} you are ${age} years old`);
-}
-hi('Marta', 17);
-hi('Kirke', 21);
-hi('Danchik', 17);
-hi('Robin', 17);
-hi();
-
-let sum = function (a,b) {
-return a+b;
-}
-
-let answer = sum(4,5);
-console.log(answer);
-console.log(sum(6, 10));
-sum(5, 6);
-
-let sub = (a, b) => {
-    return a-b;
-}
-
-console.log(sub(6, 10));
-
-let mult = (a, b) => a*b;
-
-console.log(mult(6, 10));
-
-let root = a => a*a;
-console.log(root(6));
-let cool = () => console.log('cool');
-cool();
-
-let awesome = {
-party: function (){
-console.log('PARTY')
-},
-cool: () => console.log('cool'),
-nice() {
-    console.log('nice');
-}
-
-};
-
-awesome.party();
-awesome.cool();
-awesome.nice();
-
+console.log(bool);
