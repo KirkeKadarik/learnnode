9a6d83cb3ae30c60683c4e9608e57f74ca98eadf
Kirke Kadarik <kirke.kadarik@tptlive.ee>
2025-09-29 15:40:44 +0300

    add code that works ....

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-let text = 'hello world';
-console.log(text[3]);
-
-let array = [1, 2, 3];
-let num = 2;
-num.toExponential();
-array.fill(1,3,6);
-console.log(text.length);
-console.log(Math.PI);
+console.log('hello world');
+console.log(document, window);
 
 
