2b34966f1ac72b922df1be61be5dda28b80e62d2
Kirke Kadarik <kirke.kadarik@tptlive.ee>
2025-10-13 15:54:26 +0300

    add itemlist

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <script setup>
 import { computed, ref } from 'vue';
+import ItemList from './ItemList.vue';
 let newItem = ref('');
 let i = 1;
 let items = ref([
   {id: i++, name:'leib',  isDone: true},
   {id: i++, name:'piim,', isDone: false},
   {id: i++, name:'sai', isDone: true},
+  {id: i++, name:'juust', isDone: true},
+  {id: i++, name:'sink', isDone: false},
+  
 ]);
 let code = ref(404);
 function addItem(){
-  items.value.push({id: i++, name: newItem.value,  isDone: false});
+  if(newItem.value.trim() !== '') {
+  items.value.push({id: i++, name: newItem.value.trim(),  isDone: false});
+  }
   newItem.value = '';
 }
 
@@ -26,7 +32,7 @@ let todoItems = computed(() => {
     <div class="container">
     <div class="field has-addons">
   <div class="control is-expanded">
-    <input v-model="newItem" class="input" type="text" placeholder="Add item">
+    <input v-model="newItem" class="input" type="text" placeholder="Add item" @keydown.enter="addItem">
   </div>
   <div class="control">
     <button class="button is-info" @click="addItem">
@@ -34,29 +40,11 @@ let todoItems = computed(() => {
     </button>
   </div>
   </div>
-  <h1>All items</h1>
-  <ul>
-    <li v-for="item in items">
-      <input type="checkbox" v-model="item.isDone"></input>
-      {{ item.name }}</li>
-      
-  </ul>
+ 
+<ItemList :items="items" title="All items"></ItemList>
+<ItemList :items="doneItems" title="Done items"></ItemList>
+<ItemList :items="toDoItems" title="toDo items"></ItemList>
 
-  <h1>Done Items</h1>
-  <ul>
-    <li v-for="item in DoneItems" :key="item.id">
-      <input type="checkbox" v-model="item.isDone"></input>
-      {{ item.name }}</li>
-      
-  </ul>
-
-  <h1>ToDo Items</h1>
-  <ul>
-    <li v-for="item in toDoItems" :key="item.id">
-      <input type="checkbox" v-model="item.isDone"></input>
-      {{ item.name }}</li>
-      
-  </ul>
   <input v-model="code" class="input" type="number" placeholder="error error code">
   <img :src="'https://http.cat/'+ code">
 </div>
