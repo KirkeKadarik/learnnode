49532a5656ba10896b00d6e82139d82d97133dc9
Kirke Kadarik <kirke.kadarik@tptlive.ee>
2025-10-16 15:13:07 +0300

    add tabs

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,25 @@
 <script setup>
 import { ref } from 'vue';
-import Modal from './components/Modal.vue';
-import ImgModal from './components/ImgModal.vue';
-let modalActive = ref(false);
-let modal2Active = ref(false);
-let modal3Active = ref(false);
-</script>
-<template>
-    <div class="container mt-3">
-    <button class="button is-primary mx-2" @click="modalActive=true">Open modal</button>
-    <button class="button is-warning mx-2" @click="modal2Active=true">Open modal 2</button>
-    <button class="button is-danger mx-2" @click="modal3Active=true">Open modal 3</button>
-    </div>
-<ImgModal :active="modalActive" @close="modalActive = false" url="https://placekittens.com/1280/960?r=1"></ImgModal>
-<ImgModal :active="modal2Active" @close="modal2Active = false" url="https://placekittens.com/1280/960?r=2"></ImgModal>
-<Modal :active="modal3Active" @close="modal3Active=false">
-  <div class="card">
-  <div class="card-image">
-    <figure class="image is-4by3">
-      <img
-        src="https://bulma.io/assets/images/placeholders/1280x960.png"
-        alt="Placeholder image"
-      />
-    </figure>
-  </div>
-  <div class="card-content">
-    <div class="media">
-      <div class="media-left">
-        <figure class="image is-48x48">
-          <img
-            src="https://bulma.io/assets/images/placeholders/96x96.png"
-            alt="Placeholder image"
-          />
-        </figure>
-      </div>
-      <div class="media-content">
-        <p class="title is-4">John Smith</p>
-        <p class="subtitle is-6">@johnsmith</p>
-      </div>
-    </div>
+import Tabs from './components/Tabs.vue';
+let tabTitles =[ 'Pictures', 'Music', 'Videos', 'Documents', 'Games'];
+let tabContent = [
+    'some cool Picures',
+     'some cool Music',
+      'some cool Videos',
+       'some cool Documents',
+        'some cool Games',
 
-    <div class="content">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
-      iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
-      <a href="#">#responsive</a>
-      <br />
-      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-    </div>
-  </div>
+]
+let tabActive = ref(0);
+
+function setActive(key){
+    tabActive.value = key;
+}
+</script>
+
+<template>
+<Tabs :titles="tabTitles" @change="setActive"></Tabs>
+<div class="content">
+    <h1>{{ tabContent[tabActive] }}</h1>
 </div>
-    </Modal>
 </template>
