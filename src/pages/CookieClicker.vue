0f9fbfc2f7625c70c57371f29417a3240c9b1ef5
Kirke Kadarik <kirke.kadarik@tptlive.ee>
2025-10-30 14:18:52 +0200

    finish cookie clicker

--- src/pages/CookieClicker.vue
+++ src/pages/CookieClicker.vue
@@ -1,17 +1,22 @@
 <script setup>
-
+import { ref } from 'vue';
+ 
+let cookies = ref(0);
 </script>
-
+ 
 <template>
     <div class="columns">
-        <div class="column has-background-primary">
-          
+        <div class="column has-background-primary has-text-centered">
+            <h1>{{ cookies }} cookies!</h1>
+            <div @click="cookies++">
+                <img src="https://pngimg.com/d/cookie_PNG13669.png"></img>
+            </div>
         </div>
         <div class="column has-background-warning">
-            test
+            easda
         </div>
-        <div class="column has-background-info">
-            test
+        <div class="column has-background-link">
+            tegs
         </div>
     </div>
 </template>
