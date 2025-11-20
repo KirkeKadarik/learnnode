b79a12d964fab512c704dde9149ce7ece7aca480
Kirke Kadarik <kirke.kadarik@tptlive.ee>
2025-11-20 15:30:00 +0200

    add user input ex.

--- bin/terminal.js
+++ bin/terminal.js
@@ -1,89 +1,17 @@
 function write(text) {
     process.stdout.write(text);
-}
+};
 
-write('Hello terminal\n');
-write('Hello terminal\t');
+import readline from 'readline';
 
-//text color (foreground color)
-write('\x1B[31mHello terminal\n');
-write('\x1B[32m');
-write('Hello terminal\n');
-write('\x1B[33m');
-write('Hello terminal\n');
-write('\x1B[34m');
-write('Hello terminal\n');
-write('\x1B[35m');
-write('Hello terminal\n');
-write('\x1B[36m');
-write('Hello terminal\n');
-write('\x1B[37m');
-write('Hello terminal\n');
-//background color
-write('\x1B[41mHello terminal\n');
-write('\x1B[42m');
-write('Hello terminal\n');
-write('\x1B[43m');
-write('Hello terminal\n');
-write('\x1B[44m');
-write('Hello terminal\n');
-write('\x1B[45m');
-write('Hello terminal\n');
-write('\x1B[46m');
-write('Hello terminal\n');
-write('\x1B[47m');
-write('Hello terminal\n');
-write('\x1B[0m');
-write('\n');
-//dim colour
-write('\x1B[90m');
-write('Hello terminal\n');
-write('\x1B[91mHello terminal\n');
-write('\x1B[92m');
-write('Hello terminal\n');
-write('\x1B[93m');
-write('Hello terminal\n');
-write('\x1B[94m');
-write('Hello terminal\n');
-write('\x1B[95m');
-write('Hello terminal\n');
-write('\x1B[96m');
-write('Hello terminal\n');
-write('\x1B[97m');
-write('Hello terminal\n');
-write('\x1B[0m');
-write('\n');
+const rl = readline.createInterface({
 
-//bright color
-write('\x1B[100m');
-write('Hello terminal\n');
-write('\x1B[101mHello terminal\n');
-write('\x1B[102m');
-write('Hello terminal\n');
-write('\x1B[103m');
-write('Hello terminal\n');
-write('\x1B[104m');
-write('Hello terminal\n');
-write('\x1B[105m');
-write('Hello terminal\n');
-write('\x1B[106m');
-write('Hello terminal\n');
-write('\x1B[107m');
-write('Hello terminal\n');
-write('\x1B[0m');
-write('\n');
+    input: process.stdin,
+    output: process.stdout,
+});
 
-for(let i = 0; i<256; i++) {
-    write(`\x1B[38;5;${i}m`);
-    write('#');
-}
-write('\x1B[0m');
+rl.question('what is your name?', name => {
+    console.log(`Hello ${name}!!!!`);
+    rl.close();
+});
 
-write(`\x1B[48;2;255;0;0m`);
-write('Hello terminal\n');
-write('\x1B[0m');
-
-for(let i = 0; i<256; i++) {
-    write(`\x1B[48;2;${i};${i};${i}m`);
-    write(' ');
-}
