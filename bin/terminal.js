8063afde1c1bc71414132db59a2f13743e394719
Kirke Kadarik <kirke.kadarik@tptlive.ee>
2025-11-20 15:50:59 +0200

    add inquirer

--- bin/terminal.js
+++ bin/terminal.js
@@ -7,3 +7,22 @@ import chalk from "chalk";
 write(chalk.blue.bgWhite.strikethrough('hello'));
 write(chalk.bgAnsi256(67)('color'));
 write(chalk.bgRgb(255, 28, 98)('RGB'));
+
+import inquirer from "inquirer";
+
+let answers = await inquirer.prompt([
+{
+type: 'input',
+name: 'name',
+message: 'What is your name?',
+},
+{
+type: 'input',
+name: 'age',
+message: 'What is your age?',
+},
+]);
+
+
+console.log(`Hello ${answers.name}!!!!`);
+console.log(`Hello ${answers.age}!!!!`);
