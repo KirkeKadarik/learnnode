7ca72d6114a409c3970e580196028878bb8e985f
Kirke Kadarik <kirke.kadarik@tptlive.ee>
2025-11-27 12:41:13 +0200

    add terminal hello world

--- bin/terminal.js
+++ bin/terminal.js
@@ -2,27 +2,15 @@ function write(text) {
     process.stdout.write(text);
 };
 
-import chalk from "chalk";
-
-write(chalk.blue.bgWhite.strikethrough('hello'));
-write(chalk.bgAnsi256(67)('color'));
-write(chalk.bgRgb(255, 28, 98)('RGB'));
-
-import inquirer from "inquirer";
-
-let answers = await inquirer.prompt([
-{
-type: 'input',
-name: 'name',
-message: 'What is your name?',
-},
-{
-type: 'input',
-name: 'age',
-message: 'What is your age?',
-},
-]);
+function write(text) {
+    process.stdout.write(text);
+};
+ 
+write('Hello World!');
+setTimeout(() => {
+    write('\x1b[6D');
+    write('MM24!');
+}, 5000);
 
 
-console.log(`Hello ${answers.name}!!!!`);
-console.log(`Hello ${answers.age}!!!!`);
+
